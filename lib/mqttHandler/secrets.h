2bfc5ddb9857cfc71f5ca9ff4eecfef66850da92
ondfrukt <jonas.wester@gmail.com>
2024-11-28 20:51:22 +0100

    modified:   .gitignore 	deleted:    KiCad/Exchange_ProofOfConcept/#auto_saved_files# 	deleted:    KiCad/Exchange_ProofOfConcept/Exchange_ProofOfConcept-backups/Exchange_ProofOfConcept-2024-10-11_163116.zip 	new file:   KiCad/Exchange_ProofOfConcept/Exchange_ProofOfConcept-backups/Exchange_ProofOfConcept-2024-11-27_224717.zip 	modified:   KiCad/Exchange_ProofOfConcept/Exchange_ProofOfConcept.kicad_sch 	deleted:    KiCad/Exchange_ProofOfConcept/_autosave-Exchange_ProofOfConcept.kicad_sch 	deleted:    KiCad/Exchange_ProofOfConcept/~Exchange_ProofOfConcept.kicad_pcb.lck 	deleted:    KiCad/Exchange_ProofOfConcept/~Exchange_ProofOfConcept.kicad_sch.lck 	new file:   lib/mqttHandler/secrets.h 	new file:   lib/mqttHandler/secrets.h.example

---
+++ lib/mqttHandler/secrets.h
@@ -0,0 +1,14 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi-information
+#define WIFI_SSID "Cyberspace 2.4"
+#define WIFI_PASSWORD "Abba1Abba1"
+
+// MQTT-information
+#define MQTT_SERVER "192.168.0.122"
+#define MQTT_PORT 1883
+#define MQTT_USER "mqttclient"
+#define MQTT_PASSWORD "Abba1Abba1"
+
+#endif

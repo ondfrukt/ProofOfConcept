8c59fb64198f384e9fc9ba77eb82270058d6b13f
ondfrukt <79996783+ondfrukt@users.noreply.github.com>
2025-01-20 17:13:55 +0100

    Update pulsHandler.cpp

--- src/pulsHandler.cpp
+++ src/pulsHandler.cpp
@@ -34,6 +34,7 @@ void pulsHandler(int line, bool newSHK){
     
     if(Line[line].currentLineStatus != line_pulse_dialing) {
       Line[line].setLineStatus(line_pulse_dialing);
+      lineAction(line, line_pulse_dialing);
     }
     return;
   }
